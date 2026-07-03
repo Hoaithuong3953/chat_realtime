5194a1a53822ef629e0da946d332b30e557f7ea7
TrinhThuong <trinhthuong0203@gmail.com>
2026-07-03 16:08:59 +0700

    feat(models): add BaseSoftDeleteModel with soft delete functionality

--- shared/database/models.py
+++ shared/database/models.py
@@ -1,6 +1,6 @@
 import uuid
-
 from django.db import models
+from django.utils.timezone import now
 
 class BaseModel(models.Model):
     """
@@ -16,3 +16,21 @@ class BaseModel(models.Model):
 
     class Meta:
         abstract = True
+        ordering = ['-created_at']
+
+class BaseSoftDeleteModel(BaseModel):
+    """
+    Base model with soft delete support
+    """
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+    @property
+    def is_deleted(self) -> bool:
+        return self.deleted_at is not None
+
+    def soft_delete(self):
+        self.deleted_at = now()
+        self.save(update_fields=["deleted_at", "updated_at"])
