78a346e5b638fdb4d98fc0fe21f318714a525842
TrinhThuong <trinhthuong0203@gmail.com>
2026-07-05 21:46:37 +0700

    feat(users): add RefreshTokenManager for handling refresh tokens

--- apps/users/managers/__init__.py
+++ apps/users/managers/__init__.py
@@ -2,7 +2,9 @@
 Export managers for User module
 """
 from .user_manager import UserManager
+from .refresh_tokens_manager import RefreshTokenManager
 
 __all__ = [
     "UserManager",
+    "RefreshTokenManager",
 ]
---
+++ apps/users/managers/refresh_tokens_manager.py
@@ -0,0 +1,36 @@
+from datetime import timezone
+from django.db import models
+from uuid import UUID
+
+class RefreshTokenManager(models.Manager):
+    """Manager for Refresh Token model"""
+
+    def create_token(self, user, refresh_token, expires_in):
+        return self.create(
+            user=user,
+            token=refresh_token,
+            expires_in=expires_in,
+        )
+
+    def get_valid(self, refresh_token):
+        return self.filter(
+            token=refresh_token,
+            revoked_at__isnull=True,
+            expires_in__gt=timezone.now(),
+        ).first()
+
+    def revoke(self, refresh_token):
+        return self.filter(
+            token=refresh_token,
+            revoked_at__isnull=True,
+        ).update(
+            revoked_at=timezone.now()
+        )
+
+    def revoke_all(self, user_id: UUID):
+        return self.filter(
+            user_id=user_id,
+            revoked_at__isnull=True,
+        ).update(
+            revoked_at=timezone.now()
+        )
