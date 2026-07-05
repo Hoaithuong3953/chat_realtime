9c579c92a79b718dcbc6d4888d31348ea5ac26d2
TrinhThuong <trinhthuong0203@gmail.com>
2026-07-05 21:47:05 +0700

    feat(users): add RefreshToken model for managing user refresh tokens

---
+++ apps/users/models/refresh_tokens_models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+from shared.database.models import BaseModel
+from .user_models import User
+from apps.users.managers import RefreshTokenManager
+
+class RefreshToken(BaseModel):
+    """Refresh Token model"""
+    user_id = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        unique=True,
+    )
+    refresh_token = models.CharField(max_length=255, unique=True)
+    expires_in = models.DateTimeField()
+    revoked_at = models.DateTimeField(null=True, blank=True)
+
+    objects = RefreshTokenManager()
+
+    class Meta:
+        db_table = "refresh_tokens"
+
+    def __str__(self):
+        return str(self.user_id)
