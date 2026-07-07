c1322c3e58e98289dfdbb20c0a5ea53543ccc270
TrinhThuong <trinhthuong0203@gmail.com>
2026-07-08 01:18:58 +0700

    feat(auth): implement login method with error handling for invalid credentials

--- apps/users/services/auth_service.py
+++ apps/users/services/auth_service.py
@@ -1,11 +1,17 @@
 from django.db import IntegrityError, transaction
+from django.contrib.auth.hashers import check_password
 
-from apps.users.exception import DuplicateUserException
-from apps.users.models import User
+from apps.users.exception import DuplicateUserException, InvalidCredentialsException
+from apps.users.models import User, RefreshToken
 from apps.users.dto import (
     RegisterRequest,
     RegisterResponse,
+    LoginRequest,
+    LoginResponse,
 )
+from .jwt_service import JWTService
+from .refresh_token_service import RefreshTokenService
+from shared.security.token_hasher import TokenHasher
 
 class AuthService:
     """Service layer for authentication business logic"""
@@ -38,3 +44,30 @@ class AuthService:
                 raise DuplicateUserException("username")
             
         return RegisterResponse.model_validate(user)
+    
+    @staticmethod
+    @transaction.atomic
+    def login(request: LoginRequest) -> LoginResponse:
+        user = User.objects.get_by_identify(
+            request.identifier,
+        )
+
+        if user is None:
+            raise InvalidCredentialsException()
+        
+        if not check_password(request.password, user.password):
+            raise InvalidCredentialsException()
+        
+        access_token = JWTService.generate_access_token(user_id=user.id)
+        refresh_token = RefreshTokenService.generate_refresh_token()
+
+        RefreshToken.objects.upsert_token(
+            user=user,
+            refresh_token=TokenHasher.hash_token(refresh_token),
+            expires_in=RefreshTokenService.get_refresh_expiration(),
+        )
+
+        return LoginResponse(
+            access_token=access_token,
+            refresh_token=refresh_token,
+        )
