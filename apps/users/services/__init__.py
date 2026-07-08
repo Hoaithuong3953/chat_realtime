7406f5a4d577fe1101a2d89ca0268d729fc0a420
TrinhThuong <trinhthuong0203@gmail.com>
2026-07-09 05:01:03 +0700

    refactor(security): move JWT and refresh token services to shared

--- apps/users/services/__init__.py
+++ apps/users/services/__init__.py
@@ -2,11 +2,7 @@
 Export service for User module
 """
 from .auth_service import AuthService
-from .jwt_service import JWTService
-from .refresh_token_service import RefreshTokenService
 
 __all__ = [
     "AuthService",
-    "JWTService",
-    "RefreshTokenService",
 ]
--- shared/security/__init__.py
+++ shared/security/__init__.py
@@ -2,7 +2,11 @@
 Export all security utilities
 """
 from .token_hasher import TokenHasher
+from .jwt_service import JWTService
+from .refresh_token_service import RefreshTokenService
 
 __all__ = [
     "TokenHasher",
+    "JWTService",
+    "RefreshTokenService",
 ]
