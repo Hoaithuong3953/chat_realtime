a0e60995d58f6f0cad83b83dfc504eaa4eed061f
TrinhThuong <trinhthuong0203@gmail.com>
2026-07-11 01:43:23 +0700

    refactor(accounts): move LoginSerializer from users

--- apps/accounts/serializers/login_serializers.py
+++ apps/accounts/serializers/login_serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from apps.users.constants import (
+from apps.accounts.constants import (
     IDENTIFIER_MIN_LENGTH,
     PASSWORD_MIN_LENGTH,
 )
@@ -11,7 +11,7 @@ class LoginSerializer(serializers.Serializer):
         min_length=IDENTIFIER_MIN_LENGTH,
         error_messages={
             "blank": "Email or username are required",
-            "min_length": f"Email or username must be at least {IDENTIFIER_MIN_LENGTH} characters long."
+            "min_length": f"Identifier must be at least {IDENTIFIER_MIN_LENGTH} characters long."
         },
     )
     password = serializers.CharField(
@@ -24,5 +24,5 @@ class LoginSerializer(serializers.Serializer):
     )
 
     def validate_identifier(self, value: str) -> str:
-        """Normalize identifier (Email or Username)"""
-        return value.strip()
+        """Normalize identifier input"""
+        return value.strip().lower()
--- apps/users/serializers/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Export serializers for User module
-"""
-from .login_serializers import LoginSerializer
-
-__all__ = [
-    "LoginSerializer",
-]
