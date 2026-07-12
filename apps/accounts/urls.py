7c33ff0e7854a7adc3c56d586ee2554370c7e0e3
TrinhThuong <trinhthuong0203@gmail.com>
2026-07-12 21:53:57 +0700

    feat(accounts): add RefreshView URL path for token refresh requests

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from apps.accounts.views import RegisterView, LoginView
+from apps.accounts.views import RegisterView, LoginView, RefreshView
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
+    path("refresh/", RefreshView.as_view(), name="refresh")
 ]
