3c11b001731871616b045b210b941c920b4a930b
TrinhThuong <trinhthuong0203@gmail.com>
2026-07-22 21:50:25 +0700

    refactor(migrations): reorganize chat participant migrations

--- apps/users/migrations/0001_initial.py
+++ apps/users/migrations/0001_initial.py
@@ -48,32 +48,4 @@ class Migration(migrations.Migration):
                 "ordering": ["-created_at"],
             },
         ),
-        migrations.CreateModel(
-            name="RefreshToken",
-            fields=[
-                (
-                    "id",
-                    models.UUIDField(
-                        default=uuid.uuid4,
-                        editable=False,
-                        primary_key=True,
-                        serialize=False,
-                    ),
-                ),
-                ("created_at", models.DateTimeField(auto_now_add=True)),
-                ("updated_at", models.DateTimeField(auto_now=True)),
-                ("refresh_token", models.CharField(max_length=255, unique=True)),
-                ("expires_in", models.DateTimeField()),
-                ("revoked_at", models.DateTimeField(blank=True, null=True)),
-                (
-                    "user",
-                    models.OneToOneField(
-                        on_delete=django.db.models.deletion.CASCADE, to="users.user"
-                    ),
-                ),
-            ],
-            options={
-                "db_table": "refresh_tokens",
-            },
-        ),
     ]
--- apps/users/migrations/0003_rename_date_of_birth_user_dob_user_avatar_url_and_more.py
+++ apps/users/migrations/0003_rename_date_of_birth_user_dob_user_avatar_url_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("users", "0002_delete_refreshtoken"),
+        ("users", "0001_initial"),
     ]
 
     operations = [
