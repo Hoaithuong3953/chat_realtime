85c691f8a025f2608eef9f4a2e14dfd36488779c
TrinhThuong <trinhthuong0203@gmail.com>
2026-08-01 21:19:19 +0700

    feat(websocket): validation websocket connection

---
+++ apps/chat_messages/websocket/connect_service.py
@@ -0,0 +1,20 @@
+from uuid import UUID
+
+from apps.chats.models import Chat
+from apps.chat_messages.exceptions import ChatNotFoundException, ChatAccessDeniedException
+from apps.chat_participants.models import ChatParticipant
+
+class ChatConnectService:
+    @staticmethod
+    def validate_chat(
+        chat_id: UUID,
+        user_id: UUID,
+    ) -> None:
+        chat = Chat.objects.get_chat_by_id(chat_id=chat_id)
+
+        if chat is None:
+            raise ChatNotFoundException()
+
+        is_participant = ChatParticipant.objects.is_participant(user_id=user_id, chat_id=chat_id)
+        if not is_participant:
+            raise ChatAccessDeniedException()
--- apps/chat_messages/websocket/consumers.py
+++ apps/chat_messages/websocket/consumers.py
@@ -1,21 +1,50 @@
+from django.contrib.auth.models import AnonymousUser
+from channels.db import database_sync_to_async
+
 from core.websocket.base_consumer import BaseConsumer
 from core.websocket.context import WebSocketContext
 from apps.chat_messages.websocket.events import ChatEvent
 from apps.chat_messages.websocket.handlers import SendMessageHandler
+from apps.chat_messages.websocket.connect_service import ChatConnectService
+from apps.chat_messages.exceptions import ChatAccessDeniedException, ChatNotFoundException
 
 class ChatConsumer(BaseConsumer):
+    group_name: str | None = None
+
     handlers = {
         ChatEvent.SEND_MESSAGE: SendMessageHandler(),
     }
 
     def build_context(self) -> WebSocketContext:
+        """Build the context for the WebSocket connection"""
         return WebSocketContext(
             user=self.scope["user"],
             chat_id=self.scope["url_route"]["kwargs"]["chat_id"],
         )
 
     async def connect(self) -> None:
-        self.group_name = f"chat_{self.scope['url_route']['kwargs']['chat_id']}"
+        """Handle the WebSocket connection"""
+        user = self.scope["user"]
+        if isinstance(user, AnonymousUser):
+            await self.close(code=401)
+
+        chat_id = self.scope["url_route"]["kwargs"]["chat_id"]
+        try:
+            await database_sync_to_async(
+                ChatConnectService.validate_chat
+            )(
+                chat_id=chat_id,
+                user_id=user.id,
+            )
+
+        except ChatNotFoundException:
+            await self.close(code=404)
+            return
+        except ChatAccessDeniedException:
+            await self.close(code=403)
+            return
+
+        self.group_name = f"chat_{chat_id}"
 
         await self.channel_layer.group_add(
             self.group_name,
@@ -24,12 +53,15 @@ class ChatConsumer(BaseConsumer):
         await self.accept()
 
     async def disconnect(self, code: int) -> None:
-        await self.channel_layer.group_discard(
-            self.group_name,
-            self.channel_name,
-        )
+        """Handle the WebSocket disconnection"""
+        if self.group_name is not None:
+            await self.channel_layer.group_discard(
+                self.group_name,
+                self.channel_name,
+            )
 
     async def after_handle(self, response):
+        """Perform actions after handling send message event"""
         if response.event != ChatEvent.SEND_MESSAGE:
             return False
 
@@ -44,4 +76,5 @@ class ChatConsumer(BaseConsumer):
         return True
 
     async def chat_message(self, event: dict) -> None:
+        """Receive a message from the group and send it to the WebSocket"""
         await self.send_json(event["message"])
